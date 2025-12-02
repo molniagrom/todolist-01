29d39d0bbad92efdedf0bd9aff9dcfe28e0c03ac
molnia_grom <mgrom250@gmail.com>
2025-12-02 13:05:51 +0200

    fix bag

--- src/common/utils/handleError.ts
+++ src/common/utils/handleError.ts
@@ -27,7 +27,7 @@ export const handleError = (
         }
         break
       default:
-        if (result.error.status >= 500 && result.error.status < 600) {
+        if (typeof result.error.status === "number" && result.error.status >= 500 && result.error.status < 600) {
           error = "Server error occurred. Please try again later."
         } else {
           error = JSON.stringify(result.error)
